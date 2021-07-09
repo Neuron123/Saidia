b0753a007faf7d1d6859ae3413098703a5c92287
Neuron123 <39122977+Neuron123@users.noreply.github.com>
2021-07-09 13:34:01 +0300

    Update settings.py

--- saidia/settings.py
+++ saidia/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#0vit9**3yoo237qmzu_8#xox)mc--4t=bdq=-c3rw%0+qgts8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
